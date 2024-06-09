084d3a51d01753dc6e569e9ac994659242e903ef
IDunhill7 <110286932+IDunhill7@users.noreply.github.com>
2024-06-09 04:16:06 +0300

    Add files via upload

---
+++ odus/script.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    var userID = document.getElementById('UserID').value;
+    var password = document.getElementById('PIN').value;
+    var errorMessage = document.getElementById('error-message');
+
+    if (userID.length !== 6 || password.length !== 8) {
+        errorMessage.innerHTML = 'الرجاء ادخال اسم مستخدم من 6 ارقام و رقم سري من 8 ارقام فقط';
+        return false;
+    }
+
+    // Redirect to main.html if validation passes
+    window.location.href = '/odus/main/main.html';
+    return true;
+}
